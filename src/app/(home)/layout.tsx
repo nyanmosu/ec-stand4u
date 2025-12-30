446a91d232f63ba2414dd6ec281d64766362a919
nyanmosu <nyanmosu02@gmail.com>
2025-12-30 13:01:34 +0900

    ヘッダーを前面に表示する

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,10 +7,13 @@ interface HomeLayoutProps {
 
 export default function HomeLayout({ children }: HomeLayoutProps) {
   return (
+    <div className="relative">
+      <div className="absolute inset-x-0 top-0 z-20">
         <Header
           navItems={HOME_CONFIG.homeNavItems}
           socialLinks={HOME_CONFIG.homeSocialLinks}
         />
+      </div>
       <main>{children}</main>
       <footer></footer>
     </div>
